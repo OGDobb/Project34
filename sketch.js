30b0be74f73cc08bfee88f54eed0be1ff76fb154
OGDobb <73088984+OGDobb@users.noreply.github.com>
2021-02-14 17:10:08 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 //Create variables here
-var dog, happyDog, database, foodS, foodStock;
+var dog, happyDog, database, foodS, foodStock, foodObj;
 
 function preload()
 {
@@ -15,23 +15,45 @@ function setup() {
   foodStock=database.ref('Food');
   foodStock.on("value",readStock);
 
+  feed=crateButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
   
 }
 
 
 function draw() {  
-bakground(46,139,87);
-if(keyWentDown(UP_ARROW)){
-writeStock(foodS);
-dog.addImage(happyDog);
-}
-  drawSprites();
-  //add styles here
-Text("Note: Press UP_ARROW Key to Feed Drago Milk!");
-textSize=5;
+background(46,139,87);
+//if(keyWentDown(UP_ARROW)){
+//writeStock(foodS);
+//dog.addImage(happyDog);
+display();
+textSize(15);
 fill("white");
 stroke("black");
+
+if(lastFed>=12){
+  Text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+}else if(lastFed==0){
+  Text("Last Feed : 12 AM", 350,30);
+}else{
+  Text("Last Feed : "+ lastfed + " AM", 350,30);
 }
+drawSprites();
+}
+
+
+
+ 
+  //add styles here
+//Text("Note: Press UP_ARROW Key to Feed Drago Milk!");
+
+
 
 function readStock(data){
   foodS=data.val();
@@ -40,6 +62,23 @@ function readStock(data){
 function writeStock(x){
   database.ref('/').update({
     Food:x
+  })
+}
+
+function feedDog(){
+  dog.addImage(happyDog);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
+  })
+}
+
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
   })
 }
 
