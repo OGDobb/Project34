cb07881555509c54fd969168f1d60a5063bd082a
OGDobb <73088984+OGDobb@users.noreply.github.com>
2021-08-29 22:14:01 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 //Create variables here
 var dog, happyDog, database, foodS, foodStock, foodObj;
-
+var dogImg, happyDogImg;
 function preload()
 {
   //load images here
-  dog=loadImage("images/dogImg.png");
-  happyDog=loadImage("images/dogImg1.png");
+  dogImg=loadImage("images/dogImg.png");
+  happyDogImg=loadImage("images/dogImg1.png");
 }
 
 function setup() {
@@ -15,13 +15,17 @@ function setup() {
   foodStock=database.ref('Food');
   foodStock.on("value",readStock);
 
-  feed=crateButton("Feed the dog");
-  feed.position(700,95);
-  feed.mousePressed(feedDog);
+  dog=createSprite(250,250);
+  dog.addImage("dogImg",dogImg);
+  dog.scale=0.1;
 
-  addFood=createButton("Add Food");
-  addFood.position(800,95);
-  addFood.mousePressed(addFoods);
+  // feed=createButton("Feed the dog");
+  // feed.position(700,95);
+  // feed.mousePressed(feedDog);
+
+  // addFood=createButton("Add Food");
+  // addFood.position(800,95);
+  // addFood.mousePressed(addFoods);
 
   
 }
@@ -29,21 +33,27 @@ function setup() {
 
 function draw() {  
 background(46,139,87);
-//if(keyWentDown(UP_ARROW)){
-//writeStock(foodS);
-//dog.addImage(happyDog);
-display();
+if(keyWentDown(UP_ARROW)){
+writeStock(foodS);
+dog.addImage("happyDogImg",happyDogImg);
+dog.changeImage("happyDogImg");
+}
+//display();
+
 textSize(15);
 fill("white");
 stroke("black");
+text("Note: Press UP_ARROW Key to Feed Drago Milk!",100,50);
 
-if(lastFed>=12){
-  Text("Last Feed : "+ lastFed%12 + " PM", 350,30);
-}else if(lastFed==0){
-  Text("Last Feed : 12 AM", 350,30);
-}else{
-  Text("Last Feed : "+ lastfed + " AM", 350,30);
-}
+text("Food Available: " + foodS, 200, 100);
+
+// if(lastFed>=12){
+//   Text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+// }else if(lastFed==0){
+//   Text("Last Feed : 12 AM", 350,30);
+// }else{
+//   Text("Last Feed : "+ lastfed + " AM", 350,30);
+// }
 drawSprites();
 }
 
@@ -51,7 +61,7 @@ drawSprites();
 
  
   //add styles here
-//Text("Note: Press UP_ARROW Key to Feed Drago Milk!");
+
 
 
 
@@ -60,6 +70,13 @@ function readStock(data){
 }
 
 function writeStock(x){
+  if(x<=0){
+    x=0;
+  }
+  else{
+    x=x-1;
+  }
+
   database.ref('/').update({
     Food:x
   })
